cf8fbc40e91821d3009fb49e81a1ef661f3af61b
olsenmatthew <olsenmatthew780@gmail.com>
2020-10-01 19:28:18 +1000

    added password to create_user function

--- utils/connect_to_database.py
+++ utils/connect_to_database.py
@@ -2,9 +2,9 @@ import psycopg2
 import time
 
 
-def create_user(first, last, email, conn, cur):
+def create_user(first, last, email, password, conn, cur):
 	try:
-		cmd = "INSERT INTO users(first,last,email) VALUES('{}','{}','{}');".format(first, last, email)
+		cmd = "INSERT INTO users(first,last,email,password) VALUES('{}','{}','{}', '{}');".format(first, last, email, password)
 		print(cmd)
 		cur.execute(cmd)
 		conn.commit()
@@ -12,6 +12,8 @@ def create_user(first, last, email, conn, cur):
 	except psycopg2.errors.UniqueViolation as e:
 		print(e)
 		return "Unable to create new account. Account with that email already exists."
+
+
 
 if __name__ == '__main__':
 	port = 5432
@@ -30,8 +32,9 @@ if __name__ == '__main__':
 	first = 'Matthew'
 	last = 'Olsen'
 	email = 'OlsenMatthew780@gmail.com'
+	password = 'password'
 
-	response = create_user(first, last, email, conn, cur)
+	response = create_user(first, last, email, password, conn, cur)
 	print(response)
 
 	time.sleep(1)
