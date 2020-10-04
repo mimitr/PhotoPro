284bc858ab382bb5d0c970ff580c426403dbad22
Criscoll <cristiand2021@gmail.com>
2020-10-05 00:50:41 +1100

    ignore this commit, alot of package issues, need to revert

--- photopro-app/src/components/MainWrapper.jsx
+++ photopro-app/src/components/MainWrapper.jsx
@@ -2,12 +2,14 @@ import React, { useState, useEffect } from 'react';
 
 import Toolbar from './toolbar/toolbar';
 import Feed from './feed/feed';
+import Searchbar from './searchbar/searchbar';
 
 function MainWrapper() {
   return (
     <React.Fragment>
       <Toolbar />
       <Feed />
+      <Searchbar />
     </React.Fragment>
   );
 }
--- photopro-app/src/components/searchbar/searchbar.jsx
+++ photopro-app/src/components/searchbar/searchbar.jsx
@@ -1,3 +1,17 @@
 import React from 'react';
 
-function Searchbar() {}
+import 'antd/dist/antd.css';
+import { Input } from 'antd';
+const { Search } = Input;
+
+function Hello(props) {
+  return <h1>h</h1>;
+  // <Search
+  //   placeholder="input search text"
+  //   enterButton="Search"
+  //   size="large"
+  //   onSearch={(value) => console.log(value)}
+  // />
+}
+
+export default Hello;
