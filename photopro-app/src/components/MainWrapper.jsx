7247fcfaa4375663bf6e37a6640de53f548f259d
Criscoll <cristiand2021@gmail.com>
2020-10-05 00:53:35 +1100

    reverted to old state to fix package issues

--- photopro-app/src/components/MainWrapper.jsx
+++ photopro-app/src/components/MainWrapper.jsx
@@ -2,14 +2,12 @@ import React, { useState, useEffect } from 'react';
 
 import Toolbar from './toolbar/toolbar';
 import Feed from './feed/feed';
-import Searchbar from './searchbar/searchbar';
 
 function MainWrapper() {
   return (
     <React.Fragment>
       <Toolbar />
       <Feed />
-      <Searchbar />
     </React.Fragment>
   );
 }
--- photopro-app/src/components/searchbar/searchbar.jsx
+++ photopro-app/src/components/searchbar/searchbar.jsx
@@ -1,17 +1,3 @@
 import React from 'react';
 
-import 'antd/dist/antd.css';
-import { Input } from 'antd';
-const { Search } = Input;
-
-function Hello(props) {
-  return <h1>h</h1>;
-  // <Search
-  //   placeholder="input search text"
-  //   enterButton="Search"
-  //   size="large"
-  //   onSearch={(value) => console.log(value)}
-  // />
-}
-
-export default Hello;
+function Searchbar() {}
