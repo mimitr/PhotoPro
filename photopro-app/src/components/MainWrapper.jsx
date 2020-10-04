52643792fa3bbc1a70e74399914e071a92dd877a
Criscoll <cristiand2021@gmail.com>
2020-10-04 23:15:14 +1100

    toolbar alligned and first image added

--- photopro-app/src/components/MainWrapper.jsx
+++ photopro-app/src/components/MainWrapper.jsx
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
 
 import Toolbar from './toolbar/toolbar';
+import Feed from './feed/feed';
 
 function MainWrapper() {
   return (
     <React.Fragment>
       <Toolbar />
+      <Feed />
     </React.Fragment>
   );
 }
