13894d7dc7cf50675426aab3e6108865ad40d00f
Mimi <mimitran1305@gmail.com>
2020-10-05 20:38:01 +1100

    building main age but problem with api

---
+++ photopro-app/src/components/ImageList/ImageList.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function ImageList(props) {
+  console.log(props.foundImages);
+
+  return <div>images</div>;
+}
+
+// const ImageList = (props) => {
+//   console.log(props.foundImages);
+
+//   return <div>images</div>;
+// };
+
+export default ImageList;
--- photopro-app/src/components/MainWrapper.jsx
+++ photopro-app/src/components/MainWrapper.jsx
@@ -1,11 +1,33 @@
-import React, { useState, useEffect } from 'react';
-
-import Toolbar from './toolbar/toolbar';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Toolbar from "./toolbar/toolbar";
+import SearchBar from "./SearchBar/SearchBar";
+import ImageList from "./ImageList/ImageList";
 
 function MainWrapper() {
+  const [images, setImages] = useState([]);
+
+  onSearchSubmit = async (term) => {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization: "Client-ID YOUR_ACCESS_KEYS",
+      },
+    });
+
+    setImages(response.data.results.map((x) => images.push(x)));
+  };
+
+  onSearchSubmit().then(() => {
+    console.log(images);
+  });
+
   return (
     <React.Fragment>
       <Toolbar />
+      <SearchBar userSubmit={onSearchSubmit} />
+      <span>Found: {images.length} images</span>
+      <ImageList foundImages={images} />
     </React.Fragment>
   );
 }
