36b9ea40740e220b62042396492beff36549cb73
Mimi <mimitran1305@gmail.com>
2020-10-08 19:54:47 +1100

    aligned the photos to remove the gaps

--- photopro-app/src/components/ImageSquare/ImageSquare.js
+++ photopro-app/src/components/ImageSquare/ImageSquare.js
@@ -1,10 +1,38 @@
 import React, { Component } from "react";
 
-class ImageCard extends Component {
+class ImageSquare extends Component {
+  constructor(props) {
+    super(props);
+    this.imageRef = React.createRef();
+    this.state = { spans: 0 };
+  }
+
+  /*componentDidMount() {
+    console.log(this.imageRef);
+    //console.log(this.imageRef.current.clientHeight);
+
+    setSpans = () => {
+      console.log(this.imageRef.current.clientHeight);
+    };
+  }*/
+  componentDidMount() {
+    this.imageRef.current.addEventListener("load", this.setSpans);
+    // console log the height of the images
+    console.log(this.imageRef.current.clientHeight);
+  }
+
+  setSpans = () => {
+    const height = this.imageRef.current.clientHeight;
+    //const spansRows = Math.ceil(height / 150);
+    const spansRows = Math.ceil(height / 10);
+    this.setState({ spans: spansRows });
+  };
+
   render() {
     return (
-      <div>
+      <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
         <img
+          ref={this.imageRef}
           src={this.props.image.urls.regular}
           alt={this.props.image.alt_description}
         />
@@ -13,4 +41,4 @@ class ImageCard extends Component {
   }
 }
 
-export default ImageCard;
+export default ImageSquare;
