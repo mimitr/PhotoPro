1c7e4cebecb0cc2607b58190f92d64d72bc6e2c9
mimitr <mimitran1305@gmail.com>
2020-10-12 23:31:59 +1100

    started on like buttons on the photo grids

--- photopro-app/src/components/feed/ImageCard/ImageCard.jsx
+++ photopro-app/src/components/feed/ImageCard/ImageCard.jsx
@@ -1,4 +1,8 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import "./ImageCard.css";
+import like_icon from "../../../icons/outline_favorite_border_white_18dp.png";
+import buy_icon from "../../../icons/outline_bookmark_border_white_18dp.png";
+import bookmark_icon from "../../../icons/outline_add_shopping_cart_white_18dp.png";
 
 class ImageCard extends Component {
   constructor(props) {
@@ -12,7 +16,7 @@ class ImageCard extends Component {
   }
 
   componentDidMount() {
-    this.imageRef.current.addEventListener('load', this.setSpans);
+    this.imageRef.current.addEventListener("load", this.setSpans);
   }
 
   setSpans = () => {
@@ -23,12 +27,18 @@ class ImageCard extends Component {
 
   render() {
     return (
-      <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
+      <div
+        class="photo-container"
+        style={{ gridRowEnd: `span ${this.state.spans}` }}
+      >
         <img
           ref={this.imageRef}
           src={`data:image/jpg;base64,${this.props.image.img}`}
           alt={this.props.image.caption}
         />
+        <button class="btn like-btn">Like</button>
+        <button class="btn bookmark-btn">Bookmark</button>
+        <button class="btn buy-btn">Buy</button>
       </div>
     );
   }
