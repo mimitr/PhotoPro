ceb938fd46b08256d3396cfa18a6efc0ae5f0b1a
mimitr <mimitran1305@gmail.com>
2020-10-14 23:11:03 +1100

    started implementing post redirection

--- photopro-app/src/components/feed/ImageCard/ImageCard.jsx
+++ photopro-app/src/components/feed/ImageCard/ImageCard.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import "./ImageCard.css";
-import { useHistory } from "react-router-dom";
 import { Link } from "react-router-dom";
+import { Redirect } from "react-router";
 
 class ImageCard extends Component {
   constructor(props) {
@@ -26,37 +26,43 @@ class ImageCard extends Component {
   handleImageClicked = (e) => {
     console.log("image clicked");
     //this.props.history.push("/post/:url");￼
+    <Redirect
+      to={{
+        pathname: {`/post/${this.props.image_key}`}
+      }}
+    />;
   };
 
   render() {
+    console.log(this.props);
     return (
-      <div
-        onClick={this.handleImageClicked}
-        className="photo-container"
-        style={{ gridRowEnd: `span ${this.state.spans}` }}
-      >
-        <img
-          ref={this.imageRef}
-          src={`data:image/jpg;base64,${this.props.image.img}`}
-          alt={this.props.image.caption}
-        />
-        <button className="btn like-btn" type="button">
-          <svg width="25" height="25">
-            <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"></path>
-          </svg>
-        </button>
+      <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
+        <Link to={`/post/${this.props.image_key}`}>
+          <div onClick={this.handleImageClicked} className="photo-container">
+            <img
+              ref={this.imageRef}
+              src={`data:image/jpg;base64,${this.props.image.img}`}
+              alt={this.props.caption}
+            />
+            <button className="btn like-btn" type="button">
+              <svg width="25" height="25">
+                <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"></path>
+              </svg>
+            </button>
 
-        <button className="btn bookmark-btn">
-          <svg width="25" height="25">
-            <path d="M17 3H7c-1.1 0-1.99.9-1.99 2L5 21l7-3 7 3V5c0-1.1-.9-2-2-2z"></path>
-          </svg>
-        </button>
+            <button className="btn bookmark-btn">
+              <svg width="25" height="25">
+                <path d="M17 3H7c-1.1 0-1.99.9-1.99 2L5 21l7-3 7 3V5c0-1.1-.9-2-2-2z"></path>
+              </svg>
+            </button>
 
-        <button className="btn buy-btn">
-          <svg width="25" height="25">
-            <path d="M21.99 4c0-1.1-.89-2-1.99-2H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h14l4 4-.01-18z"></path>
-          </svg>
-        </button>
+            <button className="btn buy-btn">
+              <svg width="25" height="25">
+                <path d="M21.99 4c0-1.1-.89-2-1.99-2H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h14l4 4-.01-18z"></path>
+              </svg>
+            </button>
+          </div>
+        </Link>
       </div>
     );
   }
--- photopro-app/src/components/feed/feed.jsx
+++ photopro-app/src/components/feed/feed.jsx
@@ -1,32 +1,26 @@
 import React from "react";
 import "./feed.css";
 import ImageCard from "./ImageCard/ImageCard";
-import { Link } from "react-router-dom";
 
 const Feed = (props) => {
   const imgs = props.foundImages.map((img) => {
-    return {
-      id: img.id,
-      component: (
-        <ImageCard
-          key={img.id}
-          image={img}
-          img_caption={img.caption}
-          img_uploader={img.uploader}
-          img_title={img.title}
-          img_price={img.price}
-        />
-      ),
-    };
+    return (
+      <ImageCard
+        key={img.id}
+        image={img}
+        image_key={img.id}
+        img_caption={img.caption}
+        img_uploader={img.uploader}
+        img_title={img.title}
+        img_price={img.price}
+      />
+    );
   });
 
-  console.log(imgs[0]);
   return (
     <React.Fragment>
       <h2>Found Images: {props.foundImages.length}</h2>
-      <div className="image-list">
-        <Link to={`/post/${imgs.id}`}>{imgs[0].component}</Link>
-      </div>
+      <div className="image-list">{imgs}</div>
     </React.Fragment>
   );
 };
