e3853dc20eccd3fdbdc935b27ac8534d851bce72
Ethan Lee <leewenkit@gmail.com>
2020-10-22 14:45:25 +1100

    Added new views, revised notifications functions (untested)

--- photopro-app/api/utils/database/notifications.py
+++ photopro-app/api/utils/database/notifications.py
@@ -2,9 +2,10 @@ import psycopg2
 
 
 
-def get_like_notification(image_id, conn, cur):
+def get_like_notification(user_id, timestamp, conn, cur):
     try:
-        cmd = "SELECT * FROM likes WHERE image_id={}".format(image_id)
+        cmd = "SELECT image_id, liker FROM like_notif " \
+              "WHERE uploader={} AND liker!={} AND comment_notif.create_at > {}".format(user_id, user_id, timestamp)
         cur.execute(cmd)
         conn.commit()
         result = cur.fetchall()
@@ -12,12 +13,26 @@ def get_like_notification(image_id, conn, cur):
     except Exception as e:
         return False
 
-def get_comment_notification(user_id, image_id, conn, cur):
+def get_comment_notification(user_id, timestamp, conn, cur):
     try:
-        cmd = "SELECT comment_id, commenter, comment FROM comments WHERE image_id={} AND commenter!={}".format(image_id, user_id)
+        cmd = "SELECT image_id, commenter, comment FROM comment_notif " \
+              "WHERE uploader={} AND commenter!={} AND comment_notif.create_at > {} ".format(user_id, user_id, timestamp)
         cur.execute(cmd)
         conn.commit()
         result = cur.fetchall()
+        return result()
+    except Exception as e:
+        return False
+
+def get_user_timestamp(user_id, conn, cur):
+    try:
+        cmd = "SELECT last_active FROM users WHERE id = {}".format(user_id)
+        cur.execute(cmd)
+        conn.commit()
+        result = cur.fetchone()
+        print(result)
+        result = result[0]
+        print(result)
         return result()
     except Exception as e:
         return False
