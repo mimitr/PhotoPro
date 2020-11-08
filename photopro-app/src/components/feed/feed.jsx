85bf6dd621c9eee3d3b9e18a0d462d885020e7a1
Criscoll <cristiand2021@gmail.com>
2020-11-08 19:10:25 +1100

    inf scroll issues still present, about to attempt a different approach without using the infinitescroll component

--- photopro-app/src/components/feed/feed.jsx
+++ photopro-app/src/components/feed/feed.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './feed.css';
 import axios from 'axios';
 import ImageCard from './ImageCard/ImageCard';
@@ -12,18 +12,21 @@ const fetchImages = (term, setImgs, setHasMore) => {
     url: 'http://localhost:5000/discovery',
     params: { query: term, batch_size: 20 }, //user_id: 1
   }).then((res) => {
-    if (res.data.result != null) {
+    if (res.data.result != false) {
       setHasMore(true);
-      setImgs((prevImgs) => {
-        return [...prevImgs, ...res.data.result];
-      });
+      setTimeout(() => {
+        setImgs((prevImgs) => {
+          return [...prevImgs, ...res.data.result];
+        });
+      }, 500);
 
       // setImgs(imgs.concat(res.data.result));
     } else {
+      console.log('no more images to return');
       setHasMore(false);
-      setImgs((prevImgs) => {
-        return [...prevImgs];
-      });
+      // setImgs((prevImgs) => {
+      //   return [...prevImgs];
+      // });
     }
   });
 };
@@ -34,19 +37,28 @@ const Feed = (props) => {
   const [photoIdBookmarked, setPhotoIdBookmarked] = useState(0);
   const [hasMore, setHasMore] = useState(true);
 
-  console.log(`LENGTH = ${imgs.length}`);
+  useEffect(() => {
+    setImgs([]);
+    setHasMore(false);
+  }, [props.query]);
 
-  const images = imgs.map((img) => {
-    return (
-      <ImageCard
-        key={img.id}
-        image={img}
-        openBookmarkModal={modalIsOpen}
-        setOpenBookmarkModal={setModalIsOpen}
-        setPhotoId={setPhotoIdBookmarked}
-      />
-    );
-  });
+  console.log(`LENGTH = ${imgs.length}`);
+  console.log(`HASMORE = ${hasMore}`);
+
+  let images = null;
+  if (imgs != null) {
+    images = imgs.map((img) => {
+      return (
+        <ImageCard
+          key={img.id}
+          image={img}
+          openBookmarkModal={modalIsOpen}
+          setOpenBookmarkModal={setModalIsOpen}
+          setPhotoId={setPhotoIdBookmarked}
+        />
+      );
+    });
+  }
 
   return (
     <React.Fragment>
@@ -56,7 +68,11 @@ const Feed = (props) => {
         next={fetchImages(props.query, setImgs, setHasMore)}
         hasMore={hasMore}
         loader={'Loading...'}
-        scrollThreshold="1000px"
+        endMessage={
+          <p style={{ textAlign: 'center' }}>
+            <b>Yay! You have seen it all</b>
+          </p>
+        }
       >
         <div className="image-list">{images}</div>
       </InfiniteScroll>
