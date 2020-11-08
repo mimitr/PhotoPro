40f8287025e8d363c8c1f70e11a4af03243743ec
Criscoll <cristiand2021@gmail.com>
2020-11-09 00:00:38 +1100

    inf-scroll working but very slow with some bugs that need to be worked on

--- photopro-app/src/components/feed/feed.jsx
+++ photopro-app/src/components/feed/feed.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
 import './feed.css';
 import axios from 'axios';
 import ImageCard from './ImageCard/ImageCard';
@@ -6,76 +6,102 @@ import BookmarkModal from '../modal/BookmarkModal';
 import InfiniteScroll from 'react-infinite-scroll-component';
 import { ImageSearch } from '@material-ui/icons';
 
-const fetchImages = (term, setImgs, setHasMore) => {
-  axios({
-    method: 'GET',
-    url: 'http://localhost:5000/discovery',
-    params: { query: term, batch_size: 20 }, //user_id: 1
-  }).then((res) => {
-    if (res.data.result != false) {
-      setHasMore(true);
-      setTimeout(() => {
-        setImgs((prevImgs) => {
-          return [...prevImgs, ...res.data.result];
-        });
-      }, 500);
-
-      // setImgs(imgs.concat(res.data.result));
-    } else {
-      console.log('no more images to return');
-      setHasMore(false);
-      // setImgs((prevImgs) => {
-      //   return [...prevImgs];
-      // });
-    }
-  });
-};
-
 const Feed = (props) => {
   const [imgs, setImgs] = useState([]);
   const [modalIsOpen, setModalIsOpen] = useState(false);
   const [photoIdBookmarked, setPhotoIdBookmarked] = useState(0);
   const [hasMore, setHasMore] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const observer = useRef();
+  const lastImageRef = useCallback(
+    (node) => {
+      if (loading) return;
+      if (observer.current) observer.current.disconnect();
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting && hasMore) {
+          fetchImages(props.query);
+        }
+      });
+
+      if (node) observer.current.observe(node);
+    },
+    [loading, hasMore]
+  );
 
   useEffect(() => {
     setImgs([]);
     setHasMore(false);
+    fetchImages(props.query);
   }, [props.query]);
 
-  console.log(`LENGTH = ${imgs.length}`);
-  console.log(`HASMORE = ${hasMore}`);
+  const fetchImages = (term) => {
+    setLoading(true);
 
-  let images = null;
-  if (imgs != null) {
-    images = imgs.map((img) => {
-      return (
-        <ImageCard
-          key={img.id}
-          image={img}
-          openBookmarkModal={modalIsOpen}
-          setOpenBookmarkModal={setModalIsOpen}
-          setPhotoId={setPhotoIdBookmarked}
-        />
-      );
+    axios({
+      method: 'GET',
+      url: 'http://localhost:5000/discovery',
+      params: { query: term, batch_size: 5 }, //user_id: 1
+    }).then((res) => {
+      console.log(res);
+      if (res.data.result != false) {
+        setHasMore(true);
+        setTimeout(() => {
+          setLoading(false);
+          setImgs((prevImgs) => {
+            return [...prevImgs, ...res.data.result];
+          });
+        }, 500);
+      } else {
+        console.log('no more images to return');
+        setLoading(false);
+        setHasMore(false);
+        // setImgs((prevImgs) => {
+        //   return [...prevImgs];
+        // });
+      }
     });
-  }
+  };
+
+  // console.log(`LENGTH = ${imgs.length}`);
+  // console.log(`HASMORE = ${hasMore}`);
 
   return (
     <React.Fragment>
       <h2>Found Images: {imgs.length}</h2>
-      <InfiniteScroll
-        dataLength={imgs.length}
-        next={fetchImages(props.query, setImgs, setHasMore)}
-        hasMore={hasMore}
-        loader={'Loading...'}
-        endMessage={
-          <p style={{ textAlign: 'center' }}>
-            <b>Yay! You have seen it all</b>
-          </p>
-        }
-      >
-        <div className="image-list">{images}</div>
-      </InfiniteScroll>
+
+      <div className="image-list">
+        {imgs.map((image, index) => {
+          if (image === null) {
+            return null;
+          }
+
+          if (imgs.length === index + 1) {
+            return (
+              <div key={index} ref={lastImageRef} className="bug-fix">
+                <ImageCard
+                  key={image.id}
+                  image={image}
+                  openBookmarkModal={modalIsOpen}
+                  setOpenBookmarkModal={setModalIsOpen}
+                  setPhotoId={setPhotoIdBookmarked}
+                />
+              </div>
+            );
+          } else {
+            return (
+              <ImageCard
+                key={image.id}
+                image={image}
+                openBookmarkModal={modalIsOpen}
+                setOpenBookmarkModal={setModalIsOpen}
+                setPhotoId={setPhotoIdBookmarked}
+              />
+            );
+          }
+        })}
+      </div>
+      <h2 style={{ textAlign: 'center' }}>{loading && 'Loading...'}</h2>
+
       <BookmarkModal
         openModal={modalIsOpen}
         onCloseModal={() => setModalIsOpen(false)}
