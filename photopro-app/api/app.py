f0df4050deb6e792d528ad00c1d93be1cee59ccf
olsenmatthew <olsenmatthew780@gmail.com>
2020-11-12 09:41:55 +1100

    fixed login and registration to have username

--- photopro-app/api/app.py
+++ photopro-app/api/app.py
@@ -94,14 +94,18 @@ def api_login():
     return jsonify({"result": result, "user_id": user_id})
 
 
-@app.route("/create_user")
+@app.route("/create_user", methods=["GET", "POST"])
 def api_create_user():
     first = request.args.get("first")
     last = request.args.get("last")
     email = request.args.get("email")
     password = request.args.get("password")
+    username = request.args.get("username")
+    if username is None:
+        username = str(str(first)+" "+str(last))
+    print(username)
 
-    result = create_user(first, last, email, password, conn, cur)
+    result = create_user(first, last, email, password, username, conn, cur)
     if result:
         (result, user_id) = login_user(email, password, conn, cur)
         print(result, user_id)
@@ -111,7 +115,7 @@ def api_create_user():
     return jsonify({"result": result})
 
 
-@app.route("/change_password")
+@app.route("/change_password", methods=["GET", "POST"])
 def api_change_password():
     email = request.args.get("email")
     password = request.args.get("password")
@@ -121,7 +125,7 @@ def api_change_password():
     return {"result": result}
 
 
-@app.route("/forgot_password_get_change_password_link")
+@app.route("/forgot_password_get_change_password_link", methods=["GET", "POST"])
 def api_forgot_password():
     email = request.args.get("email")
     result = forgot_password_get_change_password_link(email, conn, cur)
