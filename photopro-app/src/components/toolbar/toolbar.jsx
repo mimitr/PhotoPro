5e9d88237147221148f2909ebd203cb156e5cdc4
mimitr <mimitran1305@gmail.com>
2020-11-14 00:09:38 +1100

    added logo

--- photopro-app/src/components/toolbar/toolbar.jsx
+++ photopro-app/src/components/toolbar/toolbar.jsx
@@ -1,28 +1,29 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import './toolbar.css';
-import { useHistory } from 'react-router-dom';
-import Button from '@material-ui/core/Button';
-import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
-import { makeStyles } from '@material-ui/core/styles';
-import CloudUploadIcon from '@material-ui/icons/CloudUpload';
-import Notifications from './notifications/notifications';
+import React, { useState } from "react";
+import axios from "axios";
+import "./toolbar.css";
+import { useHistory } from "react-router-dom";
+import Button from "@material-ui/core/Button";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import { makeStyles } from "@material-ui/core/styles";
+import CloudUploadIcon from "@material-ui/icons/CloudUpload";
+import Notifications from "./notifications/notifications";
+import logo from "../../logo/photoPro-logo.png";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     margin: theme.spacing(1),
-    border: '3px solid red',
+    border: "3px solid red",
   },
   button: {
-    color: 'white',
+    color: "rgb(110, 110, 110)",
   },
 }));
 
 function Toolbar() {
   const history = useHistory();
   const classes = useStyles();
-  const loggedIn = localStorage.getItem('userLoggedIn');
+  const loggedIn = localStorage.getItem("userLoggedIn");
 
   const [anchorEl, setAnchorEl] = useState(null);
 
@@ -35,15 +36,15 @@ function Toolbar() {
   };
 
   const handleSignInClicked = () => {
-    history.push('/login');
+    history.push("/login");
   };
 
   const handleSignUpClicked = () => {
-    history.push('/signup');
+    history.push("/signup");
   };
 
   const handleProfileClicked = function () {
-    const userID = localStorage.getItem('userID');
+    const userID = localStorage.getItem("userID");
     console.log(`In toolbar the userID = ${userID}`);
     history.push({
       pathname: `/profile/${userID}`,
@@ -53,24 +54,24 @@ function Toolbar() {
 
   const handleLogoutClicked = () => {
     localStorage.clear();
-    history.push('/');
+    history.push("/");
     history.go(0); // forces the page to re-render if you are already on it which causes it to display the right information
   };
 
   const handleDiscoveryClicked = () => {
-    history.push('/');
+    history.push("/");
   };
 
   const handleUploadClicked = () => {
-    history.push('/uploadphoto');
+    history.push("/uploadphoto");
   };
 
   const handleChangePassClicked = () => {
-    history.push('/changepassword');
+    history.push("/changepassword");
   };
 
   const handleCollectionsClicked = () => {
-    const userID = localStorage.getItem('userID');
+    const userID = localStorage.getItem("userID");
     console.log(`In toolbar the userID = ${userID}`);
     history.push({
       pathname: `/collections/${userID}`,
@@ -79,18 +80,18 @@ function Toolbar() {
   };
 
   const handleCartClicked = () => {
-    history.push('/shopping-cart');
+    history.push("/shopping-cart");
   };
 
   const handleMyPurchasesClicked = () => {
-    history.push('/my-purchases');
+    history.push("/my-purchases");
   };
 
   let buttons;
-  if (loggedIn === 'true') {
+  if (loggedIn === "true") {
     buttons = (
       <React.Fragment>
-        <div className={'flex-container-buttons-2'}>
+        <div className={"flex-container-buttons-2"}>
           <Button
             className={classes.button}
             size="small"
@@ -156,10 +157,10 @@ function Toolbar() {
       <React.Fragment>
         <div className="toolbar-left-placeholder"></div>
         <div className="flex-container-buttons-1">
-          <Button style={{ color: 'white' }} onClick={handleSignInClicked}>
+          <Button className={classes.button} onClick={handleSignInClicked}>
             Sign in
           </Button>
-          <Button style={{ color: 'white' }} onClick={handleSignUpClicked}>
+          <Button className={classes.button} onClick={handleSignUpClicked}>
             Getting Started
           </Button>
         </div>
@@ -170,7 +171,7 @@ function Toolbar() {
   return (
     <React.Fragment>
       <div className="flex-container-toolbar">
-        <h1 className="toolbar-text">PhotoPro</h1>
+        <img className="logo" src={logo} alt="Logo" />
         {buttons}
       </div>
     </React.Fragment>
