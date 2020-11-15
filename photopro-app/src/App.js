510aed1ee158bd5bbb7347186088faccfcce579f
Criscoll <cristiand2021@gmail.com>
2020-11-15 13:47:40 +1100

    confirmation modal added on successful post

--- photopro-app/src/App.js
+++ photopro-app/src/App.js
@@ -5,7 +5,7 @@ import LoginPage from './pages/LoginPage';
 import RegistrationPage from './pages/RegistrationPage';
 import ProfilePage from './pages/ProfilePage';
 import PostInfo from './pages/PostInfo/PostInfo';
-import CapturePhotoPage from './pages/CapturePhotoPage';
+import CapturePhotoPage from './pages/UploadPhotoPage/CapturePhotoPage';
 import ChangePasswordPage from './pages/ChangePasswordPage';
 import ForgotPasswordPage from './pages/ForgotPasswordPage';
 import EditPostPage from './pages/EditPostPage';
--- photopro-app/src/pages/UploadPhotoPage/PostConfirmationModal/PostConfirmationModal.jsx
+++ photopro-app/src/pages/UploadPhotoPage/PostConfirmationModal/PostConfirmationModal.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import './ConfirmationModal.css';
+import './PostConfirmationModal.css';
 import Button from '@material-ui/core/Button';
 import { useHistory } from 'react-router-dom';
 
-export default function CheckoutConfirmationModal(props) {
+export default function PostConfirmationModal(props) {
   const history = useHistory();
   if (!props.isOpen) {
     return null;
@@ -14,8 +14,8 @@ export default function CheckoutConfirmationModal(props) {
         <div className="overlay-styles" />
         <div className="confirmation-modal">
           <h3>
-            You order has been approved! Please visit 'My Purchases' in your
-            profile tab to download.
+            You post has been uploaded successfully! Please check your profile
+            page under the profile tab to see your uploads.
           </h3>
           <Button
             variant="contained"
