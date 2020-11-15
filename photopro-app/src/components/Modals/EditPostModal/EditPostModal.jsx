6e71c736ca2b9afdc4689c227d90146d523a8162
mimitr <mimitran1305@gmail.com>
2020-11-16 10:41:31 +1100

    ignore

---
+++ photopro-app/src/components/Modals/EditPostModal/EditPostModal.jsx
@@ -0,0 +1,138 @@
+import React, { useState, useEffect } from "react";
+import ReactDom from "react-dom";
+import { Button, FormGroup, FormControl, FormLabel } from "react-bootstrap";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
+import "./EditPostModal.css";
+
+export default function EditPostPage(props) {
+  const [caption, set_caption] = useState("");
+  const [title, set_title] = useState("");
+  const [price, set_price] = useState("");
+  const [tags, set_tags] = useState("");
+  const { match } = props;
+  const history = useHistory();
+
+  function validate_title() {
+    return title.length > 0 && title.length < 50;
+  }
+  function validate_caption() {
+    return caption.length > 0 && caption.length < 50;
+  }
+  function validate_price() {
+    return parseInt(price) > 0 && price.length > 0;
+  }
+  function validate_tags() {
+    return tags.length > 0 && tags.length < 100;
+  }
+
+  async function edit_post(event) {
+    event.preventDefault();
+
+    var response = await axios.get("http://localhost:5000/edit_post", {
+      params: {
+        image_id: parseInt(match.params.id),
+        title: title,
+        price: price,
+        caption: caption,
+        tags: tags,
+      },
+    });
+    console.log(response);
+
+    if (response.data.result) {
+      history.goBack();
+    }
+  }
+
+  console.log("heloo");
+
+  if (!props.openModal) {
+    return null;
+  } else {
+    return ReactDom.createPortal(
+      <React.Fragment>
+        <div className="overlayStyles" />
+        <div
+          className="editPostModal"
+          onClick={(e) => {
+            e.stopPropagation();
+          }}
+        >
+          <div className="grid-container-editpost">
+            <form className="grid-item-form" onSubmit={edit_post}>
+              <FormGroup controlId="title" bsSize="large">
+                <FormLabel>Title</FormLabel>
+                <FormControl
+                  Type="title"
+                  value={title}
+                  onChange={(e) => set_title(e.target.value)}
+                />
+              </FormGroup>
+
+              <FormGroup controlId="price" bsSize="large">
+                <FormLabel>Price</FormLabel>
+                <FormControl
+                  Type="price"
+                  value={price}
+                  onChange={(e) => set_price(e.target.value)}
+                />
+              </FormGroup>
+
+              <FormGroup controlId="caption" bsSize="large">
+                <FormLabel>Caption</FormLabel>
+                <FormControl
+                  Type="caption"
+                  value={caption}
+                  onChange={(e) => set_caption(e.target.value)}
+                />
+              </FormGroup>
+
+              <FormGroup controlId="tags" bsSize="large">
+                <FormLabel>tags</FormLabel>
+                <FormControl
+                  Type="tags"
+                  value={tags}
+                  onChange={(e) => set_tags(e.target.value)}
+                />
+              </FormGroup>
+
+              <Button
+                variant="primary"
+                disabled={
+                  !validate_caption() ||
+                  !validate_title() ||
+                  !validate_price() ||
+                  !validate_tags()
+                }
+                type="submit"
+              >
+                Submit
+              </Button>
+              <Button
+                onClick={() => {
+                  history.push("/profile");
+                }}
+              >
+                Cancel
+              </Button>
+            </form>
+            <div className="grid-item-old-values">
+              <h1>Current title: {props.title}</h1>
+              <h1>Current Caption: {props.caption}</h1>
+              <h1>Current Price: ${props.price}</h1>
+              {/* <h1>Current Tags: ${props.tags}</h1> */}
+            </div>
+            <div className="grid-item-image">
+              <img
+                src={`data:image/jpg;base64,${props.url}`}
+                alt={props.caption}
+              />
+            </div>
+          </div>
+        </div>
+      </React.Fragment>,
+      document.getElementById("editPostPortal")
+    );
+  }
+}
--- photopro-app/src/components/feed/ImageCard/ImageCard.jsx
+++ photopro-app/src/components/feed/ImageCard/ImageCard.jsx
@@ -1,19 +1,20 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import './ImageCard.css';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import { withStyles } from '@material-ui/core';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import BookmarkIcon from '@material-ui/icons/Bookmark';
-import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
-import DeleteIcon from '@material-ui/icons/Delete';
-import EditIcon from '@material-ui/icons/Edit';
-import PostModal from '../../Modals/PostModal/PostModal';
-import AddedToCartModal from '../../Modals/AddedToCartModal/AddedToCartModal';
+import React, { Component } from "react";
+import axios from "axios";
+import "./ImageCard.css";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import { withStyles } from "@material-ui/core";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import BookmarkIcon from "@material-ui/icons/Bookmark";
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
+import DeleteIcon from "@material-ui/icons/Delete";
+import EditIcon from "@material-ui/icons/Edit";
+import PostModal from "../../Modals/PostModal/PostModal";
+import AddedToCartModal from "../../Modals/AddedToCartModal/AddedToCartModal";
+import EditPostModal from "../../Modals/EditPostModal/EditPostModal";
 
 const deletePostRequest = async function (imageID) {
-  const response = await axios.get('http://localhost:5000/delete_image_post', {
+  const response = await axios.get("http://localhost:5000/delete_image_post", {
     params: { image_id: String(imageID) }, //user_id: 1
   });
 
@@ -23,46 +24,46 @@ const deletePostRequest = async function (imageID) {
 // matrial-ui component style override
 const styles = {
   root: {
-    position: 'absolute',
-    bottom: '0%',
-    left: '50%',
-    color: 'rgba(255, 255, 255,1)',
-    height: '15%',
-    width: '15%',
+    position: "absolute",
+    bottom: "0%",
+    left: "50%",
+    color: "rgba(255, 255, 255,1)",
+    height: "15%",
+    width: "15%",
   },
   iconSize: {
-    width: '60%',
-    height: '60%',
+    width: "60%",
+    height: "60%",
   },
   likeSize: {
-    width: '80%',
-    height: '80%',
+    width: "80%",
+    height: "80%",
   },
   like: {
-    left: '5%',
+    left: "5%",
   },
   bookmark: {
-    left: '60%',
+    left: "60%",
   },
   buy: {
-    left: '80%',
+    left: "80%",
   },
   delete: {
-    left: '4%',
-    top: '10%',
-    width: '16%',
-    height: '20%',
-    '&:hover': {
-      backgroundColor: 'rgba(180, 65, 65, 0.82)',
+    left: "4%",
+    top: "10%",
+    width: "16%",
+    height: "20%",
+    "&:hover": {
+      backgroundColor: "rgba(180, 65, 65, 0.82)",
     },
   },
   edit: {
-    left: '82%',
-    top: '10%',
-    width: '14%',
-    height: '18%',
-    '&:hover': {
-      backgroundColor: 'rgba(6, 149, 193, 0.7)',
+    left: "82%",
+    top: "10%",
+    width: "14%",
+    height: "18%",
+    "&:hover": {
+      backgroundColor: "rgba(6, 149, 193, 0.7)",
     },
   },
 };
@@ -75,24 +76,26 @@ class ImageCard extends Component {
     // after accessing the DOM, we can get the height of each ImageCard
     this.imageRef = React.createRef();
     this.setOpenPostModal = this.setOpenPostModal.bind(this);
+    this.setOpenEditPostModal = this.setOpenEditPostModal.bind(this);
     this.setAddedToCartModal = this.setAddedToCartModal.bind(this);
     this.setCartStatus = this.setCartStatus.bind(this);
     this.setNumLikes = this.setNumLikes.bind(this);
     this.state = {
       openPostModal: false,
+      openEditPostModal: false,
       openCartAddedModal: false,
-      cartStatus: '',
+      cartStatus: "",
       spans: 0,
-      animateImages: '',
+      animateImages: "",
       numLikes: this.props.image.num_likes,
     };
   }
 
   componentDidMount() {
-    this.imageRef.current.addEventListener('load', this.setSpans);
+    this.imageRef.current.addEventListener("load", this.setSpans);
     setTimeout(() => {
       this.setState({
-        animateImages: 'image-container-animate',
+        animateImages: "image-container-animate",
       });
     }, 100);
   }
@@ -115,6 +118,10 @@ class ImageCard extends Component {
 
   setOpenPostModal = (newState) => {
     this.setState({ openPostModal: newState });
+  };
+
+  setOpenEditPostModal = (newState) => {
+    this.setState({ openEditPostModal: newState });
   };
 
   setNumLikes = (newState) => {
@@ -141,8 +148,8 @@ class ImageCard extends Component {
 
     const apiAddToCart = (imageID, setModal, setCartStatus) => {
       axios({
-        method: 'POST',
-        url: 'http://localhost:5000/add_purchase',
+        method: "POST",
+        url: "http://localhost:5000/add_purchase",
         params: {
           save_for_later: 0,
           purchased: 0,
@@ -151,28 +158,28 @@ class ImageCard extends Component {
       }).then((response) => {
         if (response.data.result !== false) {
           setModal(true);
-          setCartStatus('added');
+          setCartStatus("added");
         }
       });
     };
 
     const apiRemoveFromCart = (imageID, setModal, setCartStatus) => {
       axios({
-        method: 'POST',
-        url: 'http://localhost:5000/delete_item_from_cart',
+        method: "POST",
+        url: "http://localhost:5000/delete_item_from_cart",
         params: {
           image_id: String(imageID),
         },
       }).then((response) => {
         if (response.data.result !== false) {
           setModal(true);
-          setCartStatus('removed');
+          setCartStatus("removed");
         }
       });
     };
 
     axios({
-      url: 'http://localhost:5000/item_is_in_cart',
+      url: "http://localhost:5000/item_is_in_cart",
       params: { image_id: this.props.image.id },
     }).then((response) => {
       console.log(response);
@@ -200,19 +207,20 @@ class ImageCard extends Component {
   };
 
   handleEditClicked = (e) => {
-    this.setState({ redirect: `/editpost/${this.props.image.id}` });
     e.stopPropagation();
+    console.log("edit clicked");
+    this.setState({ openEditPostModal: true });
   };
 
   render() {
     document.body.style.overflow = this.state.openPostModal
-      ? 'hidden'
-      : 'unset';
+      ? "hidden"
+      : "unset";
 
     let component;
 
     let uploaderID = String(this.props.image.uploader);
-    let userID = localStorage.getItem('userID');
+    let userID = localStorage.getItem("userID");
 
     console.log(`uploaderID = ${uploaderID}, userID = ${userID}`);
 
@@ -316,6 +324,47 @@ class ImageCard extends Component {
               title={this.props.image.title}
               uploader={this.props.image.uploader}
               setNumLikes={this.setNumLikes}
+            />
+          </div>
+        ) : null}
+
+        {this.state.openPostModal ? (
+          <div
+            className="modal-wrapper"
+            onClick={() => {
+              this.setState({ openPostModal: false });
+            }}
+          >
+            <PostModal
+              openModal={this.state.openPostModal}
+              setOpenModal={this.setOpenPostModal}
+              imageID={this.props.image.id}
+              url={this.props.image.img}
+              caption={this.props.image.caption}
+              price={this.props.image.price}
+              title={this.props.image.title}
+              uploader={this.props.image.uploader}
+              setNumLikes={this.setNumLikes}
+            />
+          </div>
+        ) : null}
+
+        {this.state.openEditPostModal ? (
+          <div
+            className="modal-wrapper"
+            onClick={() => {
+              this.setState({ openEditPostModal: false });
+            }}
+          >
+            <EditPostModal
+              openModal={this.state.openEditPostModal}
+              setOpenModal={this.setOpenEditPostModal}
+              imageID={this.props.image.id}
+              url={this.props.image.img}
+              caption={this.props.image.caption}
+              price={this.props.image.price}
+              title={this.props.image.title}
+              uploader={this.props.image.uploader}
             />
           </div>
         ) : null}
