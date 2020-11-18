ac80ab04b561765a2cedaeec697f431bcc7393c0
mimitr <mimitran1305@gmail.com>
2020-11-18 14:41:15 +1100

    working on uploading

--- photopro-app/src/pages/ProfilePage.jsx
+++ photopro-app/src/pages/ProfilePage.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
-import Toolbar from '../components/toolbar/toolbar';
-import UserPhotos from '../components/userPhotos/UserPhotos';
-import Collections from '../components/collections/collections';
+import React from "react";
+import Toolbar from "../components/toolbar/toolbar";
+import UserPhotos from "../components/userPhotos/UserPhotos";
+import Collections from "../components/collections/collections";
+import Button from "@material-ui/core/Button";
+import "./ProfilePage.css";
 
 function ProfilePage(props) {
   if (props.location.state === undefined) {
@@ -14,16 +16,26 @@ function ProfilePage(props) {
   } else {
     // This variable helps certain components decide what information they should display.
     const displayMyProfile =
-      localStorage.getItem('userID') === props.location.state.uploaderID
+      localStorage.getItem("userID") === props.location.state.uploaderID
         ? true
         : false;
 
     // Tries to move the page to the top when the profile page is rendered
-    document.body.style.overflow = 'unset';
+    document.body.style.overflow = "unset";
     return (
       <React.Fragment>
         <Toolbar />
-        <div className="profile-wrapper" style={{ marginTop: '20%' }}>
+        <div className="profile-wrapper" style={{ marginTop: "20%" }}>
+          <div className="users-profile-pic">
+            <div className="dp-photo-wrapper">
+              <div className="dp-photo"></div>
+            </div>
+
+            <div className="upload-dp-button" style={{ textAlign: "center" }}>
+              <Button variant="outlined">Update Profile Photo</Button>
+            </div>
+          </div>
+
           {displayMyProfile ? null : (
             <Collections
               userID={props.location.state.uploaderID}
@@ -32,7 +44,7 @@ function ProfilePage(props) {
           )}
           <UserPhotos
             userID={props.location.state.uploaderID}
-            userLoggedIn={localStorage.getItem('userLoggedIn')}
+            userLoggedIn={localStorage.getItem("userLoggedIn")}
           />
         </div>
       </React.Fragment>
