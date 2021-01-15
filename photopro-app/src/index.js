69b5c8b4a6d385f47d57258daa4c1456c26f9155
Criscoll <cristiand2021@gmail.com>
2021-01-15 12:52:46 +1100

    removed strict mode in react index file

--- photopro-app/src/index.js
+++ photopro-app/src/index.js
@@ -6,11 +6,9 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 
 ReactDOM.render(
-  // <React.StrictMode>
-    <Router>
-      <App />
-    </Router>,
-  // </React.StrictMode>,
+  <Router>
+    <App />
+  </Router>,
   document.getElementById('root')
 );
 
